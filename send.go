183e323f0e63d0f3bfc5b5a32c2748150939e1ec
Azizi Khoirul Haq <azizikhoirulhaq@gmail.com>
2019-12-20 14:52:18 +0700

    Create send.go

---
+++ send.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://admin:Azizi1988!@167.71.202.66:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"inact-cloud-message-from-golang", // name
+		false,                             // durable
+		false,                             // delete when unused
+		false,                             // exclusive
+		false,                             // no-wait
+		nil,                               // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Message from INACT Cloud - golang"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
+}
